3714364212957448acc4ac39fc4852272e6d604e
IvanTurkov <lond51@rambler.ru>
2018-06-29 08:20:01 +0300

    Final

--- core/inc/_comments.php
+++ core/inc/_comments.php
@@ -4,7 +4,6 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/core/classes/Db.php';
 $db = new Db();
 $comments = $db->getComments();
 ?>
-<?if($comments){?>
     <table>
         <thead>
             <tr>
@@ -23,17 +22,16 @@ $comments = $db->getComments();
             </tr>
         </thead>
         <tbody>
-        <?foreach ($comments as $comment):?>
-            <tr>
-                <td><?=$comment['user_name']?></td>
-                <td><?=$comment['user_email']?></td>
-                <td><?=$comment['review_tags']?></td>
-                <td><?=$comment['date']?></td>
-                <td><?=$comment['review_text']?></td>
-            </tr>
-        <?endforeach;?>
+        <?if($comments){?>
+            <?foreach ($comments as $comment):?>
+                <tr>
+                    <td><?=$comment['user_name']?></td>
+                    <td><?=$comment['user_email']?></td>
+                    <td><?=$comment['review_tags']?></td>
+                    <td><?=$comment['date']?></td>
+                    <td><?=$comment['review_text']?></td>
+                </tr>
+            <?endforeach;?>
+        <?}?>
         </tbody>
     </table>
-<?}else{?>
-    <p>Сообщений не найдено</p>
-<?}?>
